1c3ea75bd1eded1be04731b4e4f06bd043314204
songjimin <ssong503503@gmail.com>
2024-04-29 09:21:03 +0900

    LRS UI common style

---
+++ assets/js/_layout.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $(".CONTAINER.for-teacher header.INTEGRATION").load("../../tea/_layout/_layout.html");    
+});
---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+/**
+ * clickListAddClass : list 클릭 시 class 추가 (ex. 메뉴)
+ * @param {*} target list
+ * @param {*} classNm class 이름
+ */
+const clickListAddClass = (target, classNm) => {
+    target?.forEach(list => {
+        list.addEventListener('click', (e) => {
+            target.forEach(list => list.classList.remove(classNm))
+            e.currentTarget.classList.add(classNm);
+        })
+    })
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+    const menuList = document.querySelectorAll('.menuList > li:has(ul) > a');
+    const menuLink = document.querySelectorAll('.menuList li:not(:has(> ul))');
+
+    menuList?.forEach(a => {
+        a.addEventListener('click', (e) => {
+            const aChildrenUl = a.nextElementSibling.children;
+            let hasIsCurrent = false;
+
+            for (let i = 0; i < aChildrenUl.length; i++) {
+                if (aChildrenUl[i].classList.contains('isCurrent')) {
+                    hasIsCurrent = true;
+                    break;
+                }
+            }
+
+            if(!hasIsCurrent) e.currentTarget.parentElement.classList.toggle('isOpen')
+        })
+    })
+
+    clickListAddClass(menuLink, 'isCurrent');
+})
